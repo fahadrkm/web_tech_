1d8828268d7b6e46ab80e2acf730730754984299
fahadrkm <fahadrkm@gmail.com>
2025-05-30 23:35:07 +0530

    js basics

---
+++ javascript coding/variables.js
@@ -0,0 +1,13 @@
+var x = 2;   // Allowed
+let x = 3;   // Not allowed
+
+{
+let x = 2;   // Allowed
+let x = 3;   // Not allowed
+}
+
+{
+let x = 2;   // Allowed
+var x = 3;   // Not allowed
+}
+<h5>Redeclaring a variable with let, in another block, IS allowed:</h5>
